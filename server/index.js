4d8da5d590161c24ee645cb391405b234050676b
AliBasicCoder <aliahmedreda34@gmail.com>
2022-06-24 18:09:25 +0200

    ..

--- server/index.js
+++ server/index.js
@@ -476,5 +476,7 @@ function generateToken(id) {
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("Connected to DB");
-  app.listen(5000, () => console.log("started at http://localhost:5000"));
+  app.listen(process.env.PORT || 5000, () =>
+    console.log("started at http://localhost:5000")
+  );
 });
